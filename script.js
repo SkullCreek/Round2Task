a9ba463a7ed5b09c89673c1a1354a446e263bc30
Darpanarayan Bahadur <darpan.bahadur.2001@gmail.com>
2022-02-02 18:12:51 +0530

    loading bar check

---
+++ script.js
@@ -0,0 +1,74 @@
+window.onload = () => {
+    let loader_con = document.getElementById("loader-con");
+    let load = document.getElementById("loader");
+    load.style.padding = "10000%";
+    load.style.animation = "none";
+    loader_con.style.animation = "fade .5s";
+    loader_con.style.display = "none";
+}
+
+
+const carousel_scroll = document.getElementById("carousel-scroll");
+const carousel = document.getElementById("carousel");
+const article2 = document.getElementsByClassName("article");
+const next = document.getElementById("right");
+const prev = document.getElementById("left");
+
+let count = 1;
+
+const size = carousel_scroll.clientWidth/5;
+
+carousel_scroll.style.transform = `translateX(${-size * count}px)` ;
+
+next.onclick = () => {
+    if(count >= article2.length - 1) return;
+    carousel_scroll.style.transition = "transform 0.4s ease-in-out";
+    count++;
+    carousel_scroll.style.transform = `translateX(${-size * count}px)` ;
+}
+
+prev.onclick = () => {
+    if (count <= 0) return;
+    carousel_scroll.style.transition = "transform 0.4s ease-in-out";
+    count--;
+    carousel_scroll.style.transform = `translateX(${-size * count}px)` ;
+}
+
+carousel_scroll.ontransitionend = () => {
+    if(article2[count].id === 'last'){
+        carousel_scroll.style.transition = "none";
+        count = article2.length - 2;
+        carousel_scroll.style.transform = `translateX(${-size * count}px)` ;
+    }
+    if(article2[count].id === 'first'){
+        carousel_scroll.style.transition = "none";
+        count = article2.length - count;
+        carousel_scroll.style.transform = `translateX(${-size * count}px)` ;
+    }
+}
+
+
+//scrollup
+
+function scroll(){
+    const btn = document.getElementById("up");
+    btn.onclick = () => {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth"
+        });
+    }
+}
+scroll();
+
+
+console.log(window.pageYOffset);
+window.onscroll = () => {
+    if(window.pageYOffset >= 200){
+        document.getElementById("up").style.display = "grid";
+    }
+    else{
+        document.getElementById("up").style.display = "none";
+    }
+}
